8cc6b7393ed2a21b297cbaec937a0870dca97373
vidalsa3nz <vidsaenz23@gmail.com>
2024-02-20 15:00:58 -0600

    Declaring initial variables and setting up device

--- lab6_code/lab6_code.ino
+++ lab6_code/lab6_code.ino
@@ -1,9 +1,14 @@
 void setup() {
   // put your setup code here, to run once:
-
+  cont int sensor_pin = 0;
+  int pulse_signal = 0;
+  float BPM = 0;
+  Serial.begin(9600);
 }
 
 void loop() {
+  pulse_signal = analogRead(sensor_pin);
+  Serial.println(pulse_signal);
   // put your main code here, to run repeatedly:
 
 }
