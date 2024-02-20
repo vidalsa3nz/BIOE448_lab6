5656c14f1b2a20fedae9fb6ca3d202daa04d657c
vidalsa3nz <vidsaenz23@gmail.com>
2024-02-20 16:00:44 -0600

    Fixed Errors with Flags and Checkpoint Code, and added print of BPM

--- lab6_code/lab6_code.ino
+++ lab6_code/lab6_code.ino
@@ -7,8 +7,10 @@ bool first_peak_detected = false;
 unsigned long first_pulse_time = 0;
 unsigned long second_pulse_time = 0;
 
-int upper_threshold = A;
-int lower_threshold = B;
+int upper_threshold = 950;
+int lower_threshold = 650;
+
+float pulse_period = 0;
 
 
 
@@ -19,20 +21,24 @@ void setup() {
 
 void loop() {
   pulse_signal = analogRead(sensor_pin);
-  Serial.println(pulse_signal);
+  //Serial.println(pulse_signal);
   // put your main code here, to run repeatedly:
   delay(50);
   if (pulse_signal > upper_threshold && any_peak_detected == false){
     any_peak_detected = true;
     // Do somethign about this peak
-  }
-  if (pulse_signal < lower_threshold){
-    any_peak_detected = false;
     if (first_peak_detected == false){
       first_pulse_time = millis();
+      first_peak_detected = true;
     } else {
       second_pulse_time = millis();
       pulse_period = second_pulse_time - first_pulse_time;
+      first_peak_detected = false;
     }
   }
+  if (pulse_signal < lower_threshold){
+    any_peak_detected = false;
+    
+  }
+  Serial.println(60000/pulse_period);
 }
